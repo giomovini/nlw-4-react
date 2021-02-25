714b68dd2779d0c84f3f0907e8a67a388f344ff1
Vinicius Giomo <63116407+giomovini@users.noreply.github.com>
2021-02-25 15:58:29 -0300

    Add files via upload

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,48 +1,21 @@
-import { useState, useEffect, useContext} from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/Countdown.module.css';
 
-let contdownTimeout: NodeJS.Timeout;
 
-export function Countdown () {
-    const { startNewChallenge } = useContext(ChallengesContext);
+export function Countdown() {
+    const { 
+        minutes, 
+        seconds, 
+        hasFinished, 
+        isActive, 
+        startCountdown, 
+        resetCountdown,
+    } = useContext(CountdownContext)
 
-    const [time, setTime] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    // 25 --> '2' '5'
-    // 5 --> '0' '5'
 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-
-    // mostra se o cronometro está ativo
-    function startCountdown() {
-        setIsActive(true);
-    }
-
-    function resetCountdown() {
-        clearTimeout(contdownTimeout);
-        setIsActive(false);
-        setTime(0.1 * 60);
-    }
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            contdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000)
-        } else if(isActive && time === 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time]);
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');    
 
     return(
         <div>
